afc8140e7bac44a20a64380fc98825eff0bdf964
Texly <chillforai@gmail.com>
2026-03-28 20:28:12 +0530

    Update sitemap.ts

--- api/sitemap.ts
+++ api/sitemap.ts
@@ -1,12 +1,37 @@
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "application/xml");
+import { createClient } from "@supabase/supabase-js";
 
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-      <loc>https://www.texly.online/</loc>
-    </url>
-  </urlset>`;
+export default async function handler(req, res) {
+  try {
+    const supabase = createClient(
+      process.env.SUPABASE_URL,
+      process.env.SUPABASE_KEY
+    );
 
-  res.status(200).send(xml);
+    const { data, error } = await supabase
+      .from("articles")
+      .select("slug");
+
+    if (error) throw error;
+
+    const urls = data.map(
+      (item) => `
+      <url>
+        <loc>https://www.texly.online/${item.slug}</loc>
+      </url>`
+    );
+
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>https://www.texly.online/</loc>
+      </url>
+      ${urls.join("")}
+    </urlset>`;
+
+    res.setHeader("Content-Type", "application/xml");
+    res.status(200).send(xml);
+
+  } catch (err) {
+    res.status(500).send("Error generating sitemap");
+  }
 }
