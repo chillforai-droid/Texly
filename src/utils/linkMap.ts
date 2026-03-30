e3edbe5a5a65999d09ad76037d18474f9ef277f7
Texly <chillforai@gmail.com>
2026-03-30 21:31:41 +0530

    Create linkMap.ts

---
+++ src/utils/linkMap.ts
@@ -0,0 +1,7 @@
+export const linkMap: Record<string, string> = {
+  "remove extra spaces": "/tool/remove-extra-spaces-online",
+  "remove duplicate lines": "/tool/remove-duplicate-lines-tool",
+  "text formatter": "/blog/text-formatter-online",
+  "clean text": "/tool/clean-text-online-free",
+  "word counter": "/tool/word-counter-online-free"
+};
