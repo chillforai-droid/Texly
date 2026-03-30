70d9011965b9c08c8fe1b14efcece593851d8c77
Texly <chillforai@gmail.com>
2026-03-30 21:38:10 +0530

    Update autoLink.ts

--- src/utils/autoLink.ts
+++ src/utils/autoLink.ts
@@ -1,13 +1,16 @@
+import { linkMap } from "./linkMap";
+
 export function autoLinkContent(content: string) {
+  if (!content) return content;
+
   let updated = content;
 
   Object.entries(linkMap).forEach(([keyword, url]) => {
     const regex = new RegExp(`\\b(${keyword})\\b`, "gi");
 
-    updated = updated.replace(
-      regex,
-      `<a href="${url}" class="text-blue-600 underline">$1</a>`
-    );
+    updated = updated.replace(regex, (match) => {
+      return `<a href="${url}" class="text-blue-600 underline">${match}</a>`;
+    });
   });
 
   return updated;
