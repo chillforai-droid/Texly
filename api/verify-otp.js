10ac66411441ed77f24ce4bf3d4b64565634f36f
Texly <chillforai@gmail.com>
2026-03-30 09:33:20 +0530

    Update verify-otp.js

--- api/verify-otp.js
+++ api/verify-otp.js
@@ -1,11 +1,15 @@
-let otpStore = {}; // same store (important)
+import { otpStore } from "./send-otp";
 
 export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
+
   const { email, otp } = req.body;
 
   if (otpStore[email] == otp) {
     delete otpStore[email];
-    return res.json({ success: true });
+    return res.status(200).json({ success: true });
   }
 
   return res.status(400).json({ error: "Invalid OTP" });
